1b82d1976de812956cc134a429a62d2ab8b3eba9
Holden Karau <holden@pigscanfly.ca>
2014-06-14 15:53:49 -0700

    We can read from ES, isn't that fun :)

--- src/main/scala/com/holdenkarau/esspark/ReIndexTweets.scala
+++ src/main/scala/com/holdenkarau/esspark/ReIndexTweets.scala
@@ -4,12 +4,15 @@
 
 package com.holdenkarau.esspark
 
+// Scala imports
+import scala.collection.JavaConversions._
 // Spark imports
 import org.apache.spark._
 import org.apache.spark.SparkContext._
 import org.apache.spark.SparkConf
 // ES imports
 import org.elasticsearch.hadoop.mr.EsOutputFormat
+import org.elasticsearch.hadoop.mr.EsInputFormat
 import org.elasticsearch.hadoop.cfg.ConfigurationOptions
 // Hadoop imports
 import org.apache.hadoop.mapred.{FileOutputCommitter, FileOutputFormat, JobConf, OutputFormat}
@@ -23,33 +26,41 @@ object ReIndexTweets {
       m.put(new Text(k), new Text(v))
     (NullWritable.get, m)
   }
+  def mapWritableToInput(in: MapWritable): Map[String, String] = {
+    in.map{case (k, v) => (k.toString, v.toString)}.toMap
+  }
 
   def main(args: Array[String]) {
-    if (args.length < 5) {
-      System.err.println("Usage IndexTweetsLive <master> <key> <secret key> <access token> <access token secret>  <es-resource> <es-nodes>")
+    if (args.length < 3) {
+      System.err.println("Usage IndexTweetsLive <master> <es-resource> <es-nodes>")
       System.exit(-1)
     }
 
     println("Using master" + args(0))
-    println("Using es write resource " + args(5))
-    println("Using nodes "+args(6))
+    println("Using es write resource " + args(1))
+    println("Using nodes "+args(2))
     val conf = new SparkConf
     conf.setMaster(args(0))
     val sc = new SparkContext(conf)
-    val tweets = sc.parallelize(List(Map("user" -> "holdenkarau",
-      "message" -> "stress pandas")))
-    val output = tweets.map(mapToOutput)
     val jobConf = new JobConf(sc.hadoopConfiguration)
     jobConf.set("mapred.output.format.class", "org.elasticsearch.hadoop.mr.EsOutputFormat")
     jobConf.setOutputCommitter(classOf[FileOutputCommitter])
-    jobConf.set(ConfigurationOptions.ES_RESOURCE_WRITE, args(5))
-    jobConf.set(ConfigurationOptions.ES_NODES, args(6))
+    jobConf.set(ConfigurationOptions.ES_RESOURCE_READ, args(1))
+    jobConf.set(ConfigurationOptions.ES_RESOURCE_WRITE, args(1))
+    jobConf.set(ConfigurationOptions.ES_NODES, args(2))
     // This tells the ES MR output format to use the spark partition index as the node index
     // This will degrade performance unless you have the same partition layout as ES in which case
     // it should improve performance.
     // Note: this is currently implemented as kind of a hack.
     jobConf.set("es.sparkpartition", "true")
     FileOutputFormat.setOutputPath(jobConf, new Path("-"))
-    output.saveAsHadoopDataset(jobConf)
+    // RDD of input
+    val currentTweets = sc.hadoopRDD(jobConf, classOf[EsInputFormat[Object, MapWritable]], classOf[Object], classOf[MapWritable])
+    // Extract only the map
+    // Convert the MapWritable[Text, Text] to Map[String, String]
+    val tweets = currentTweets.map{ case (key, value) => mapWritableToInput(value) }
+    println(tweets.collect().mkString(":"))
+    // Save the output
+    // output.saveAsHadoopDataset(jobConf)
   }
 }
