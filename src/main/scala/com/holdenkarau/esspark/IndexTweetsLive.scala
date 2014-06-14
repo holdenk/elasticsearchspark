575f732d7aee704e65b155ba33c9883131478952
Holden Karau <holden@pigscanfly.ca>
2014-06-13 22:48:05 -0700

    Extract arguments nad make es-nodes optional because I'm lazy

--- src/main/scala/com/holdenkarau/esspark/IndexTweetsLive.scala
+++ src/main/scala/com/holdenkarau/esspark/IndexTweetsLive.scala
@@ -17,11 +17,22 @@ import org.elasticsearch.hadoop.cfg.ConfigurationOptions;
 object IndexTweetsLive {
   def main(args: Array[String]) {
     if (args.length < 5) {
-      System.err.println("Usage IndexTweetsLive <master> <key> <secret key> <access token> <access token secret>  <es-resource> <es-nodes>")
+      System.err.println("Usage IndexTweetsLive <master> <key> <secret key> <access token> <access token secret>  <es-resource> [es-nodes]")
     }
     val master = args(0)
+    val twitterKey = args(1)
+    val twitterSecret = args(2)
+    val twitterAccessToken = args(3)
+    val twitterAccessSecret = args(4)
+    val esResource = args(5)
+    val esNodes = args.length match {
+        case x: Int if x > 6 => args(6)
+        case _ => "localhost"
+      }
+
     val ssc = new StreamingContext(master, "IndexTweetsLive", Seconds(1))
     val tweets = TwitterUtils.createStream(ssc, None)
+
     ssc.start()
     ssc.awaitTermination()
   }
