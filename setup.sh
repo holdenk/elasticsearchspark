79e049ecf955d627099b97b2aff2bd05a9587cc1
Holden Karau <holden@pigscanfly.ca>
2014-06-16 15:40:55 -0700

    Setup our schema

---
+++ setup.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+sudo /etc/init.d/elasticsearch start
+curl -XPUT 'http://localhost:9200/twitter/tweet/_mapping' -d '
+{
+    "tweet" : {
+        "properties" : {
+            "message" : {"type" : "string", "store" : true },
+            "hashTags" : {"type" : "string", "store" : true },
+            "location" : {"type" : "geo_point", "store" : true }
+        }
+    }
+}
+'
