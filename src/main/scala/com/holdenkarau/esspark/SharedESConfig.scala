49a7aae88b597540ca7b367d50d11ae19fd8275b
Holden Karau <holden@pigscanfly.ca>
2014-06-16 14:35:07 -0700

    Use some shared utilities which is the whole point anyways

---
+++ src/main/scala/com/holdenkarau/esspark/SharedESConfig.scala
@@ -0,0 +1,45 @@
+/**
+ * Shared utilities to convert setup elastic search
+ */
+
+package com.holdenkarau.esspark
+
+// Scala imports
+import scala.collection.JavaConversions._
+// Spark imports
+import org.apache.spark._
+import org.apache.spark.SparkContext._
+import org.apache.spark.SparkConf
+// ES imports
+import org.elasticsearch.hadoop.mr.EsOutputFormat
+import org.elasticsearch.hadoop.mr.EsInputFormat
+import org.elasticsearch.hadoop.cfg.ConfigurationOptions
+// Hadoop imports
+import org.apache.hadoop.mapred.{FileOutputCommitter, FileOutputFormat, JobConf, OutputFormat}
+import org.apache.hadoop.fs.Path
+import org.apache.hadoop.io.{MapWritable, Text, NullWritable}
+
+
+object SharedESConfig {
+  def setupEsOnSparkContext(sc: SparkContext, esResource: String, esNodes: Option[String],
+    esSparkPartition: Boolean = false) = {
+    val jobConf = new JobConf(sc.hadoopConfiguration)
+    jobConf.set("mapred.output.format.class", "org.elasticsearch.hadoop.mr.EsOutputFormat")
+    jobConf.setOutputCommitter(classOf[FileOutputCommitter])
+    jobConf.set(ConfigurationOptions.ES_RESOURCE_READ, esResource)
+    jobConf.set(ConfigurationOptions.ES_RESOURCE_WRITE, esResource)
+    esNodes match {
+      case Some(node) => jobConf.set(ConfigurationOptions.ES_NODES, node)
+      case _ => // Skip it
+    }
+    // This tells the ES MR output format to use the spark partition index as the node index
+    // This will degrade performance unless you have the same partition layout as ES in which case
+    // it should improve performance.
+    // Note: this is currently implemented as kind of a hack.
+    if (esSparkPartition) {
+      jobConf.set("es.sparkpartition", "true")
+    }
+    FileOutputFormat.setOutputPath(jobConf, new Path("-"))
+    jobConf
+  }
+}
---
+++ src/main/scala/com/holdenkarau/esspark/SharedIndex.scala
@@ -0,0 +1,48 @@
+/**
+ * Shared utilities to convert a tweet to something we want to index
+ */
+
+package com.holdenkarau.esspark
+
+// Scala imports
+import scala.collection.JavaConversions._
+import scala.collection.immutable.HashMap
+// Hadoop imports
+import org.apache.hadoop.io.{MapWritable, Text, NullWritable}
+// twitter imports
+import twitter4j.Status
+import twitter4j.TwitterFactory
+
+object SharedIndex {
+  // twitter helper methods
+  def prepareTweets(tweet: twitter4j.Status) = {
+    val fields = tweet.getGeoLocation() match {
+        case null => HashMap("docid" -> tweet.getId().toString, "content" -> tweet.getText())
+        case _ => HashMap("docid" -> tweet.getId().toString, "content" -> tweet.getText())
+      }
+    mapToOutput(fields)
+  }
+
+  def setupTwitter(consumerKey: String, consumerSecret: String, accessToken: String, accessTokenSecret: String) ={
+    // Set up the system properties for twitter
+    System.setProperty("twitter4j.oauth.consumerKey", consumerKey)
+    System.setProperty("twitter4j.oauth.consumerSecret", consumerSecret)
+    System.setProperty("twitter4j.oauth.accessToken", accessToken)
+    System.setProperty("twitter4j.oauth.accessTokenSecret", accessTokenSecret)
+  }
+
+  def fetchTweets(ids: Seq[String]) = {
+    val twitter = new TwitterFactory().getInstance();
+  }
+  // hadoop helper methods
+  def mapToOutput(in: Map[String, String]): (Object, Object) = {
+    val m = new MapWritable
+    for ((k, v) <- in)
+      m.put(new Text(k), new Text(v))
+    (NullWritable.get, m)
+  }
+  def mapWritableToInput(in: MapWritable): Map[String, String] = {
+    in.map{case (k, v) => (k.toString, v.toString)}.toMap
+  }
+
+}
